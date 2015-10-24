489680902b92f14325bf3fd81801c02d711a7fa9
Norbert Naskov <dilurme@gmail.com>
2015-10-24 18:58:40 +0100

    rebranded index html

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -20,3 +20,25 @@ app.factory('myHttpInterceptor', function($rootScope, $q) {
 app.config(function($httpProvider) {
   $httpProvider.interceptors.push('myHttpInterceptor');
 });
+
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
+
+})(jQuery); // End of use strict
