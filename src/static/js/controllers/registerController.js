9fd0cad34a590f56433d08efdd538246a9be288e
Norbert Naskov <dilurme@gmail.com>
2015-10-24 15:00:52 +0100

    fixed registration, fixed validation

---
+++ src/static/js/controllers/registerController.js
@@ -0,0 +1,29 @@
+app.controller('RegisterController', ['$scope', '$http', function($scope, $http) {
+    $scope.$watch('housename', function(newValue, oldValue) {
+        $http({
+            method: "GET",
+            url: "/requests/checkhousename",
+            params: {
+                'houseName': housename
+            }
+        }).success(function (data) {
+            $scope.houseNameExists = data.exists;
+        });
+    });    
+    
+    $scope.submit = function(){
+        var data = {'firstName': $scope.firstname, 'lastName': $scope.lastname, 'houseName': $scope.housename};
+        
+        var res = $http.post('/register', data);
+        
+        res.success(function(data, status, headers, config) {
+            alert("Registration successful, "+ $scope.firstname+"!");
+            window.location.replace(data.redirect); 
+        });
+        
+        res.error(function(data, status, headers, config) {
+            alert("The query can't be processed at the moment. Please try again later.");
+        });
+    };
+}]);
+
