2c5efc49627709ea833016bf93d7a756a6e8622f
domsmith94 <domsmith94@gmail.com>
2015-10-26 16:22:38 +0000

    [fix] checking for null value in main api req

--- src/app/requests.py
+++ src/app/requests.py
@@ -35,10 +35,10 @@ class TaskHandler(webapp2.RequestHandler):
             task_dict["frequency"] = task.frequency
             task_dict["taskStyle"] = task.style
 
-            most_recent = task.most_recent.get()
-            
-            task_dict["positiveFeedback"] = most_recent.positive_feedback
-            task_dict["negativeFeedback"] = most_recent.negative_feedback
+            if task.most_recent:
+                most_recent = task.most_recent.get()
+                task_dict["positiveFeedback"] = most_recent.positive_feedback
+                task_dict["negativeFeedback"] = most_recent.negative_feedback
 
             datastore_tasks.append(task_dict)
 
