37c4680042de642f5dff8d4042c75e886511f336
domsmith94 <domsmith94@gmail.com>
2015-10-26 16:40:54 +0000

    sending everCompleted bool in main task req handler

--- src/app/requests.py
+++ src/app/requests.py
@@ -34,11 +34,14 @@ class TaskHandler(webapp2.RequestHandler):
             task_dict["dateModified"] = str(task.date_modified)
             task_dict["frequency"] = task.frequency
             task_dict["taskStyle"] = task.style
+            task_dict["everCompleted"] = False
 
             if task.most_recent:
+                task_dict["everCompleted"] = True
                 most_recent = task.most_recent.get()
                 task_dict["positiveFeedback"] = most_recent.positive_feedback
                 task_dict["negativeFeedback"] = most_recent.negative_feedback
+
 
             datastore_tasks.append(task_dict)
 
