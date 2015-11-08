d5289755873bf630e812c306a2a0ac8b5bf9d235
Norbert Naskov <dilurme@gmail.com>
2015-11-08 00:03:09 +0000

    animation transition between page loading

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,7 +2,7 @@
 
 var index_app = angular.module('app.index',[]);
 
-var app = angular.module('app.dashboard', ['ui.router', 'ui.bootstrap','ui.bootstrap.tpls','angular.vertilize']);
+var app = angular.module('app.dashboard', ['ui.router', 'ui.bootstrap','ui.bootstrap.tpls','angular.vertilize', 'angularMoment', 'ngAnimate']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/dashboard");
@@ -39,3 +39,27 @@ app.config(function($stateProvider, $urlRouterProvider) {
         })
     ;
 });
+
+app.factory('taskService', function ($http, $q, $timeout) {
+    return {
+        loadTasks: function (data) {
+            var defer = $q.defer();
+            
+            $http({
+                method: 'GET',
+                url: '/requests/task'
+            }).then(function successCallback(response) {
+                    // successful http request, resolve after two seconds
+                    $timeout(function() {
+                        defer.resolve(response);
+                    }, 500)
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+                return defer.promise;
+        }
+            
+        };
+    }
+);
--- src/static/js/controllers/tasksController.js
+++ src/static/js/controllers/tasksController.js
@@ -1,21 +1,17 @@
-app.controller('TasksController', ['$scope', '$http', function($scope, $http) {
-    $http({
-        method: 'GET',
-        url: '/requests/task'
-    }).then(function successCallback(response) {
-    	var data = response.data;
-    	var date;//10:00 11:00
+app.controller('TasksController', ['$scope', '$http', 'moment', 'taskService', function($scope, $http, moment, taskService) {
+    
+    taskService.loadTasks().then(function(response) {
+        var data = response.data;
 
-		for(i=0; i<data.length;i++){
-    		data[i].dateModified = moment(data[i].dateModified, "YYYY-MM-DD HH:mm:ss");
-    		data[i].duration = (data[i].frequency * 60 * 60) + "s";
+        for(i=0; i<data.length;i++){
+            data[i].dateModified = moment(data[i].dateModified, "YYYY-MM-DD HH:mm:ss");
+            data[i].duration = (data[i].frequency * 60 * 60) + "s";
 
-    		//Works out starting point for colour change.
-    		data[i].delay = (data[i].dateModified.valueOf() - moment().valueOf())/1000 + "s";
-    	}
+            //Works out starting point for colour change.
+            data[i].delay = (data[i].dateModified.valueOf() - moment().valueOf())/1000 + "s";
+        }
         $scope.list = response.data;
-    }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
     });
+    
+    
 }]);
