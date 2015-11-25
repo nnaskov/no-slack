096c6e52bccc5ed49ea9b07b05510cdf9cb2517c
Norbert Naskov <dilurme@gmail.com>
2015-11-25 14:57:04 +0000

    history of task events popover works

--- src/static/js/directives/historyPopover.js
+++ src/static/js/directives/historyPopover.js
@@ -1,64 +1,27 @@
-app.directive('historyPopover', ['$http', function($http) {
+app.directive('historyPopover', ['$http', '$compile', '$templateCache', '$log', function($http, $compile, $templateCache, $log) {
     return {
         restrict: 'EA',
-        transclude: true,
+        replace: true,
         scope: {
             taskId: '@'
         },
         template:
-        '<span ng-transclude></span>'+
         '<span popover-placement="bottom" ' +
-        '      uib-popover="eventHistory.html" ' +
+        '      uib-popover-template="\'../static/partials/eventHistory.html\'" ' +
         '      popover-trigger="click" ' +
         '      popover-title="History" ' +
         '      class="glyphicon glyphicon-list glyph-button" aria-hidden="true">' +
         '</span>',
-        
-        controller:function($scope){
+        link: function(scope, elem, attrs) {
+            scope.taskEvents = null;
             $http({
                 method: 'GET',
-                params: {'taskID': $scope.taskId},
-                url: '/requests/task/' + $scope.taskId + '/taskevent'
+                params: {'taskID': scope.taskId},
+                url: '/requests/task/' + scope.taskId + '/taskevent'
             }).then(function successCallback(response) {
-                $scope.taskEvents = response.data;          
+                scope.taskEvents = response.data;    
+               
             });
         }
     };
 }]);
-
-/*
-app.directive('historyPopover', ['$http', '$compile', '$templateCache', '$q', function($http, $compile, $templateCache, $q) {
-    var getTemplate = function(contentType) {
-        var def = $q.defer();
-        var template = $templateCache.get("eventHistory.html");
-        if (typeof template === "undefined") {
-            $http.get("templateId.html")
-                .success(function(data) {
-                $templateCache.put("eventHistory.htm", data);
-                def.resolve(data);
-            });
-        } else {
-            def.resolve(template);
-        }
-        return def.promise;
-    };
-
-    return {
-        restrict: "A",
-        scope: {
-            item: "=" // what needs to be passed to the template
-        },
-        link: function(scope, element, attrs) {
-            getTemplate().then(function(popOverContent) {
-                var options = {
-                    content: $compile($(popOverContent))(scope),
-                    placement: "bottom",
-                    html: true,
-                    trigger: "hover"
-                };
-                $(element).popover(options);
-
-            });
-        }
-    };
-}]);*/
