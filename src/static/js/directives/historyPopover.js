906206acc3725f8e7fc17d60b6a32cb08fb446df
Norbert Naskov <dilurme@gmail.com>
2015-11-25 15:53:24 +0000

    history popover formatted template, fixed max-width

--- src/static/js/directives/historyPopover.js
+++ src/static/js/directives/historyPopover.js
@@ -10,18 +10,28 @@ app.directive('historyPopover', ['$http', '$compile', '$templateCache', '$log', 
         '      uib-popover-template="\'../static/partials/eventHistory.html\'" ' +
         '      popover-trigger="click" ' +
         '      popover-title="History" ' +
+        '      popover-append-to-body="true" ' +
         '      class="glyphicon glyphicon-list glyph-button" aria-hidden="true">' +
         '</span>',
         link: function(scope, elem, attrs) {
             scope.taskEvents = null;
-            $http({
-                method: 'GET',
-                params: {'taskID': scope.taskId},
-                url: '/requests/task/' + scope.taskId + '/taskevent'
-            }).then(function successCallback(response) {
-                scope.taskEvents = response.data;    
-               
+            
+            scope.loadHistory = function(){
+                $http({
+                    method: 'GET',
+                    params: {'taskID': scope.taskId},
+                    url: '/requests/task/' + scope.taskId + '/taskevent'
+                }).then(function successCallback(response) {
+                    scope.taskEvents = response.data;  
+                    scope.taskEvents.map((event) => (event.date = new Date(event.date).getTime()));
+                });
+            };
+            
+            elem.bind('click', function() {
+                scope.loadHistory();    
             });
+            
+            scope.loadHistory();
         }
     };
 }]);
