41f0b0ce67870770608e5dd6f5009b3dec508e12
Norbert Naskov <dilurme@gmail.com>
2015-11-28 20:14:29 +0000

    channel api integration

--- src/static/js/controllers/dashboardController.js
+++ src/static/js/controllers/dashboardController.js
@@ -1,10 +1,21 @@
 app.controller('DashboardController', ['$scope', 'channelClientID', '$log', function($scope, channelClientID, $log){
-    var onMessage = function(data) {
-        var json = JSON.parse(data['data']);
-        $scope.$broadcast(json.eventType, json);
+    var cleanMessage;
+    var onOpened = function(){
+        $log.log("BOO");
+    }
+    onMessage = function(message){      
+        cleanMessage = JSON.parse(message.data);
+        $scope.$broadcast(cleanMessage.eventType, cleanMessage);
+        $scope.$apply();        
+    }
+
+    onError = function(error){
+        $log.log(error.description);
     }
     
-    $scope.channel = new goog.appengine.Channel(channelClientID);
-    $scope.socket = $scope.channel.open();
-    $scope.socket.onmessage = onMessage;
+    var channel = new goog.appengine.Channel(channelClientID);
+    var socket = channel.open();
+    socket.onopen = onOpened;
+    socket.onmessage = onMessage;
+    socket.onerror = onError;
 }]);
