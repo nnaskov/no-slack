f92031248cc429ece51d2996f5d6151bba6c5a32
Norbert Naskov <dilurme@gmail.com>
2015-11-28 20:51:47 +0000

    automatically reobtains token if timed out

--- src/static/js/controllers/dashboardController.js
+++ src/static/js/controllers/dashboardController.js
@@ -1,21 +1,3 @@
-app.controller('DashboardController', ['$scope', 'channelClientID', '$log', function($scope, channelClientID, $log){
-    var cleanMessage;
-    var onOpened = function(){
-        $log.log("BOO");
-    }
-    onMessage = function(message){      
-        cleanMessage = JSON.parse(message.data);
-        $scope.$broadcast(cleanMessage.eventType, cleanMessage);
-        $scope.$apply();        
-    }
-
-    onError = function(error){
-        $log.log(error.description);
-    }
-    
-    var channel = new goog.appengine.Channel(channelClientID);
-    var socket = channel.open();
-    socket.onopen = onOpened;
-    socket.onmessage = onMessage;
-    socket.onerror = onError;
+app.controller('DashboardController', ['$scope', 'channelClientID', '$log', 'channelService', function($scope, channelClientID, $log, channelService){
+    channelService.openChannel(channelClientID);
 }]);
