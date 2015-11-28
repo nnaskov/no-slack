add5193e947debf276bc9349c7d10a34cc860271
Dari-K <darius.chris.k@gmail.com>
2015-11-28 16:58:57 +0000

    Delegator fix

--- src/app/delegator.py
+++ src/app/delegator.py
@@ -7,8 +7,11 @@ def delegate_task(task):
     least_full = 1000
     largest_bin = 0
 
-    for member_key in members:
-        member = member_key.get()
+    if total_diff == 0:
+        total_diff = 1
+
+    for member in members:
+        member_key = member.key
         bin_size = round(5 + (1 - (member.difficulty_done/total_diff*len(members)))*20)
         bin_perc_full = member.difficulty_assigned/bin_size
         if bin_perc_full <= least_full and bin_size > largest_bin:
