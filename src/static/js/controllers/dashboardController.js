bb2717e57aa9d170600054c10cf6b0ac428e2465
Norbert Naskov <dilurme@gmail.com>
2015-11-28 21:48:11 +0000

    taskEvent updates with channel api now

--- src/static/js/controllers/dashboardController.js
+++ src/static/js/controllers/dashboardController.js
@@ -1,3 +1,5 @@
 app.controller('DashboardController', ['$scope', 'channelClientID', '$log', 'channelService', function($scope, channelClientID, $log, channelService){
+    //dispatching events down to the children
     channelService.openChannel(channelClientID);
+    //event types: addTask, deleteTask, editTask, taskEvent, taskFeedback
 }]);
--- src/static/js/controllers/taskController.js
+++ src/static/js/controllers/taskController.js
@@ -1,4 +1,16 @@
 app.controller('TaskController', ['$scope', '$http', '$element', 'taskService', function($scope, $http, $element, taskService) {    
+    var addTaskEventCb = function(response){
+        $element.css("background-color", "#00A600");
+        $element.css("animation", "none");
+
+        $scope.feedback.positive = 0;
+        $scope.feedback.negative = 0;
+
+        $scope.task.userFeedback = null;
+
+        $scope.task.everCompleted = true;
+    }
+    
     //listeners
     $scope.$on('taskFeedback', function(ev, args){
         if(args.taskId === $scope.task.taskID){
@@ -8,23 +20,22 @@ app.controller('TaskController', ['$scope', '$http', '$element', 'taskService', 
         }
     });
     
+    $scope.$on('taskEvent', function(ev, args){
+        if(args.taskId === $scope.task.taskID){
+            addTaskEventCb({});
+            $scope.$apply();
+        }
+    });
+    
     $scope.feedback = {
         positive: $scope.task.positiveFeedback ? $scope.task.positiveFeedback : 0,
         negative: $scope.task.negativeFeedback ? $scope.task.negativeFeedback : 0,
     };
     
+    
+    
     $scope.addTaskEvent = function(){
-        taskService.addTaskEvent($scope.task.taskID).then(function(response){
-            $element.css("background-color", "#00A600");
-            $element.css("animation", "none");
-
-            $scope.feedback.positive = 0;
-            $scope.feedback.negative = 0;
-            
-            $scope.task.userFeedback = null;
-            
-            $scope.task.everCompleted = true;
-        });
+        taskService.addTaskEvent($scope.task.taskID).then(addTaskEventCb);
 	};
 
     $scope.addFeedback = function(positive){
