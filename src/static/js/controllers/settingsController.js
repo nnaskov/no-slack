7e3fdf08692d97bedcd318ea47d93ceb101c80ef
Norbert Naskov <dilurme@gmail.com>
2015-12-18 18:30:47 +0000

    settings file

--- src/static/js/controllers/settingsController.js
+++ src/static/js/controllers/settingsController.js
@@ -1,74 +1,74 @@
-app.controller('SettingsController', ['$scope', '$http', function($scope, $http) {
+app.controller('SettingsController', ['$scope', '$http', 'Upload', function ($scope, $http, Upload) {
     $scope.tenantName = "Loading...";
     $scope.originalTenantName = undefined;
-    
+
     $scope.houseName = "Loading...";
     $scope.houseNameExists = false;
     $scope.originalHouseName = undefined;
-    
-    $scope.file = "static/img/blank-picture.jpg"; //default avatar
-    
-    
-    
-    $scope.$watch('$parent.userName', function(newValue, oldValue) {
+
+    $scope.$watch('$parent.userName', function (newValue, oldValue) {
         $scope.tenantName = newValue;
-        if($scope.originalTenantName === undefined && newValue!=="Loading..."){
+        if ($scope.originalTenantName === undefined && newValue !== "Loading...") {
             $scope.originalTenantName = newValue;
         }
     });
-    
-    $scope.$watch('tenantName', function(newValue, oldValue) {
-        if(newValue !== undefined){
+
+    $scope.$watch('tenantName', function (newValue, oldValue) {
+        if (newValue !== undefined) {
             $scope.$parent.userName = newValue;
         }
     });
-    
-    $scope.$watch('$parent.houseName', function(newValue, oldValue) {
+
+    $scope.$watch('$parent.houseName', function (newValue, oldValue) {
         $scope.houseName = newValue;
-        if($scope.originalHouseName === undefined && newValue!=="Loading..."){
+        if ($scope.originalHouseName === undefined && newValue !== "Loading...") {
             $scope.originalHouseName = $scope.houseName;
         }
     });
 
-    $scope.$watch('houseName', function(newValue, oldValue) {
-        if(newValue!==$scope.originalHouseName && newValue!=="Loading..."){
+    $scope.$watch('houseName', function (newValue, oldValue) {
+        if (newValue !== $scope.originalHouseName && newValue !== "Loading...") {
             $http({
                 method: "GET",
                 url: "/requests/house/check/" + newValue,
             }).success(function (data) {
                 $scope.houseNameExists = data.exists;
 
-                if(newValue !== undefined && !$scope.houseNameExists){
+                if (newValue !== undefined && !$scope.houseNameExists) {
                     $scope.$parent.houseName = newValue;
                 }
-            }); 
-        }
-        else{
+            });
+        } else {
             $scope.$parent.houseName = newValue;
             $scope.houseName = newValue;
         }
     });
-    
-    $scope.submit = function(){
+
+    $scope.submit = function () {
         var firstName = $scope.tenantName.split(" ")[0];
         var lastName = $scope.tenantName.split(" ")[1];
         var houseName = $scope.houseName;
-        var avatar;
+        var avatar = $scope.file;
+
+        var upload = Upload.upload({
+            url: '/requests/member',
+            method: 'PUT',
+            data: {avatar: avatar, firstName: firstName, lastName: lastName, notificationsOn: true, houseName: houseName}
+        });
         
-        var data = {'firstName': firstName, 'lastName': lastName, 'houseName': houseName, avatar:undefined, notificationsOn: true};
-
-        var res = $http.put('/requests/member', data);
-
-        res.success(function(data, status, headers, config) {
-            alert("Success.");
-        });
-
-        res.error(function(data, status, headers, config) {
-            alert("The query can't be processed at the moment. Please try again later.");
-        });
+        // returns a promise
+        /*upload.then(function(resp) {
+            // file is uploaded successfully
+            console.log('file ' + resp.config.data.file.name + 'is uploaded successfully. Response: ' + resp.data);
+        }, function(resp) {
+            // handle error
+        }, function(evt) {
+            // progress notify
+            console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.data.file.name);
+        });*/
     };
-    
-    $scope.dismiss = function(){
+
+    $scope.dismiss = function () {
         $scope.tenantName = $scope.originalTenantName;
         $scope.houseName = $scope.originalHouseName;
     }
