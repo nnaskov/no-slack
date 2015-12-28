4df3460c953a6866a1492ee51637eb70abf1c3cb
Norbert Naskov <dilurme@gmail.com>
2015-12-28 19:55:40 +0000

    fix house

--- src/static/js/controllers/houseController.js
+++ src/static/js/controllers/houseController.js
@@ -1,7 +1,60 @@
-app.controller('HouseController', ['$scope', '$http', 'houseData', 'tasks', 'cssInjector', function($scope, $http, houseData, tasks, cssInjector) {
+app.controller('HouseController', ['$scope', '$http', 'houseData', 'tasks', 'cssInjector', 'userID', function($scope, $http, houseData, tasks, cssInjector, userID) {
     $scope.houseData = houseData;
     $scope.tasks = tasks;
     
     cssInjector.removeAll();
     cssInjector.add("../static/bower_components/angular-chart.js/dist/angular-chart.min.css");
+    
+    //For the donought chart
+    $scope.data = [];
+    $scope.labels = [];
+
+    //House Data is an Map
+    // Key = userID
+    // Value = Map of values for the user
+
+    //For the thumbs bar chart
+    $scope.seriesNames = ['Positive', 'Negative'];
+    $scope.labelsBar = [];
+    $scope.dataBar = [];
+    $scope.$watch('analysisUserChartTaskEventPerTask', function() {
+        $http({
+            method: "GET",
+            //The parameter userid must be the id of the loaded user.
+            //TODO Bogomil please add the userid on click from different users on house page.
+            params: {charttype: "userchart", userid: userID},
+            url: "/requests/analysis",
+        }).success(function (data) {
+
+            //For the donought chart
+            for(var i = 0; i < data.taskeventspertask.length; i ++){
+                $scope.data[i] = data.taskeventspertask[i].value;
+                $scope.labels[i] = data.taskeventspertask[i].name;
+            }
+
+            //For the bar chart
+            $scope.labelsBar = data.feedbackevents.labels;
+            $scope.dataBar = [data.feedbackevents.positive, data.feedbackevents.negative]
+
+
+
+        });
+    });
+
+    // This is testing for bar charts
+
+
+
+
+    //TODO: Have to deal with the colors. Currently it is displayed in a panel-red and looks very ugly
+    //Colors for the thumbs bar chart - green/ red
+    $scope.colorsBar = ['#42A76B', '#8A0801'];
+
+
+    //$scope.options = {
+    //    tooltipEvents: [],
+    //    onAnimationComplete: function () {
+    //        this.showTooltip(this.segments, true);
+    //},
+    //};
 }]);
