42dcdff50c4ccfb846777bb05c2b79abc852e5c0
Norbert Naskov <dilurme@gmail.com>
2015-12-29 15:42:17 +0000

    task details modal and route added

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var index_app = angular.module('app.index', []);
 
 var app = angular.module('app.dashboard', ['ui.router', 'ui.bootstrap', 'ui.bootstrap.tpls', 'ui.sortable', 'ngAnimate', 'ui-iconpicker', 'chart.js', 'ngSanitize', 'ngProgress', 'angularMoment', 'ngFileUpload', 'angular-web-notification', 'angular.css.injector']);
@@ -18,15 +17,34 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             },
             reloadOnSearch: false,
         })
+        .state('dashboard.loadTask', {
+            url: "/task/load/:taskID",
+            onEnter: function ($stateParams, $state, $uibModal) {
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    templateUrl: 'static/partials/taskDetails.html',
+                    
+                });
+                modalInstance.result.then(function success() {
+                    //Do success things
+                }, function fail(reason) {
+                    if (~reason.indexOf('backdrop')) {
+                        $state.go("dashboard", null);
+                    }
+                });
+            }
+        })
         .state("dashboard.addTask", {
-            url: "/add/task",
+            url: "/task/add",
             onEnter: function ($stateParams, $state, $uibModal) {
                 var modalInstance = $uibModal.open({
                     animation: true,
                     templateUrl: 'static/partials/taskForm.html',
                     controller: 'TaskFormController',
                     resolve: {
-                        task: function(){ return null; }
+                        task: function () {
+                            return null;
+                        }
                     }
                 });
                 modalInstance.result.then(function success() {
@@ -39,14 +57,16 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             }
         })
         .state("dashboard.editTask", {
-            url: "/edit/task/:taskID",
+            url: "/task/edit/:taskID",
             onEnter: function ($stateParams, $state, $uibModal, task) {
                 var modalInstance = $uibModal.open({
                     animation: true,
                     templateUrl: 'static/partials/taskForm.html',
                     controller: 'TaskFormController',
                     resolve: {
-                        task: function(){return task;}
+                        task: function () {
+                            return task;
+                        }
                     }
                 });
                 modalInstance.result.then(function success() {
@@ -83,7 +103,7 @@ app.config(function ($stateProvider, $urlRouterProvider) {
         });
 });
 
-app.run(function($rootScope, ngProgressFactory) {    
+app.run(function ($rootScope, ngProgressFactory) {
     $rootScope.$on("$stateChangeStart", function () {
         $rootScope.progressbar = ngProgressFactory.createInstance();
         $rootScope.progressbar.setColor("#000");
