43251d350907f9af69db68c5e27e578b799300df
Norbert Naskov <dilurme@gmail.com>
2015-12-29 20:51:58 +0000

    automatically determine frequency unit for add and edit task

---
+++ src/static/js/controllers/taskDetailController.js
@@ -0,0 +1,8 @@
+app.controller('TaskFormController', ['$scope', '$state', '$http', '$uibModalInstance', '$state', '$stateParams', '$log','taskService', 'task', 'cssInjector' , function ($scope, $state, $http, $uibModalInstance, $state, $stateParams, $log, taskService, task, cssInjector) {
+    cssInjector.removeAll();
+    
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+        $state.go("dashboard", null);
+    };
+}]);
