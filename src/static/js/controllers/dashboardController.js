94e1549ba86cc2940e521d76d93204dbc09c51be
Norbert Naskov <dilurme@gmail.com>
2015-12-30 12:37:35 +0000

    fix notifications delete add task

--- src/static/js/controllers/dashboardController.js
+++ src/static/js/controllers/dashboardController.js
@@ -88,6 +88,7 @@ app.controller('DashboardController', ['$scope', 'channelClientID', 'userID', '$
                     icon: 'static/img/notification.png',
                     autoClose: 4000 
                 }, function () {});
+                $scope.notificationsStatus = true;
 
             } else if(args.eventType == "taskEvent") {
                 $scope.notifications.push({
@@ -100,6 +101,7 @@ app.controller('DashboardController', ['$scope', 'channelClientID', 'userID', '$
                     icon: 'static/img/notification.png',
                     autoClose: 4000 
                 }, function () {});
+                $scope.notificationsStatus = true;
             }
             else if (args.eventType == "addTask") {
                 $scope.notifications.push({
@@ -127,7 +129,7 @@ app.controller('DashboardController', ['$scope', 'channelClientID', 'userID', '$
                 }, function () {});
             }
 
-            $scope.notificationsStatus = true;
+            
         }
     };
 
