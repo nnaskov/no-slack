0657428790a9c666c20b55cca4f61d7f5d0fdb9a
domsmith94 <domsmith94@gmail.com>
2016-01-04 11:51:15 +0000

    removing unused unit tests

--- src/main.py
+++ src/main.py
@@ -52,41 +52,7 @@ class IndexHandler(webapp2.RequestHandler):
         self.response.out.write(INDEX_HTML)
 
 
-class UnitTestsHandler(webapp2.RequestHandler):
-    """
-     This class is used for visually showing the results of the unittests.
-     To use it open:
-     http://localhost:8080/tests
-     http://no-slack.appspot.com/tests
-    """
-    from app.unittests.test_models import BlankDatastoreTestCase, LocalDatastoreTesetCase
-    from app.unittests.jsons_test import JSONSTestCase
-
-    # List of all unittest.TestCase classes to be run
-    unitTestClasses = [BlankDatastoreTestCase, LocalDatastoreTesetCase, JSONSTestCase]
-
-    def get(self):
-        import app.external.HTMLTestRunner as HTMLTestRunner
-        import unittest
-
-        # Creating HTMLTest Runner
-        runner = HTMLTestRunner.HTMLTestRunner(
-            stream=self.response,
-            title='No-Slack Unit Test',
-            description='All unit tests in the app.'
-        )
-
-        # creating a new test suite
-        newSuite = unittest.TestSuite()
-
-        for testCase in self.unitTestClasses:
-            # adding a test case
-            newSuite.addTest(unittest.makeSuite(testCase))
-        runner.run(newSuite)
-
-
 app = webapp2.WSGIApplication([
     ('/', HomePageHandler),
     (r'/index/?', IndexHandler),
-    (r'/tests/?', UnitTestsHandler)
 ], debug=True)
